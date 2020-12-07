5ec6b2279f55b6081d8c7e9379e087427463fb45
zayka130192 <zayka130192@gmail.com>
2020-12-07 16:30:14 +0300

    added styles for the slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
-$(function() {
-
+$(function () {
+  $(".slider__items").slick({
+    slidesToShow: 3,
+    adaptiveHeight: true,
+    arrows: true,
+    dots: false,
+    prevArrow: '<button class="slick-arrow slick-prew"><img src="images/prev-arrow.png"></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/next-arrow.png"></button>',
+  });
 });
