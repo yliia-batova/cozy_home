ee07abb9b84166d71e3c57ada94e0719cfc6823c
zayka130192 <zayka130192@gmail.com>
2020-12-23 17:03:57 +0300

    added adaptive index.html

--- app/js/main.js
+++ app/js/main.js
@@ -6,5 +6,23 @@ $(function () {
     dots: false,
     prevArrow: '<button class="slick-arrow slick-prew"><img src="images/prev-arrow.png"></button>',
     nextArrow: '<button class="slick-arrow slick-next"><img src="images/next-arrow.png"></button>',
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 2,          
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,          
+        }
+      }
+    ]
+  });
+
+  $('.header__menu-btn').on('click', function(){
+    $('.header__menu .menu').slideToggle();
   });
 });
